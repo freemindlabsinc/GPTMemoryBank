ac03666f36e0db366a11af1b842cee9f1a6262b8
Alessandro Federici <afederici75@users.noreply.github.com>
2024-01-17 09:20:22 -0500

    ...

--- app/main.py
+++ app/main.py
@@ -2,9 +2,10 @@ from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dependencies import get_query_token, get_token_header
 from internal import admin
-from routers import items, users, memory
+from routers import items, users, memory, wellknown
+from fastapi.openapi.utils import get_openapi
 
-app = FastAPI(dependencies=[Depends(get_query_token)])
+app = FastAPI()#dependencies=[Depends(get_query_token)])
 
 origins = [
     "*",
@@ -18,7 +19,28 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(wellknown.wellknown)
 app.include_router(memory.router)
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Custom title",
+        version="2.5.0",
+        summary="This is a very custom OpenAPI schema",
+        description="Here's a longer description of the custom **OpenAPI** schema",
+        routes=app.routes,
+        servers=[{ "url": "https://bbe3-100-34-174-93.ngrok-free.app" }]
+    )    
+    openapi_schema["info"]["x-logo"]  = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
 
 #app.include_router(users.router)
 #app.include_router(items.router)
@@ -31,9 +53,9 @@ app.include_router(memory.router)
 #)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello Bigger Applications!"}
+#@app.get("/")
+#async def root():
+#    return {"message": "Hello Bigger Applications!"}
 
 import uvicorn
 if __name__ == "__main__":
--- app/routers/memory.py
+++ app/routers/memory.py
@@ -1,4 +1,36 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
+from pydantic import BaseModel, Field
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/memory",
+    tags=["memory"],
+    #dependencies=[Depends(get_token_header)],
+    responses={404: {"description": "Not found"}},    
+)
+
+fake_memory = [ ]
+
+class StoreRequest(BaseModel):
+    memory: str
+    topic: str = None
+    
+class QueryRequest(BaseModel):
+    query: str
+    topic: str = None    
+
+@router.post("/save", summary="Saves a memory in the memory bank", operation_id="saveMemory")
+async def save_memory(store_request: StoreRequest):
+    """
+    Saves a memory in the memory bank
+    """
+    fake_memory.append(store_request.memory)
+    return store_request;
+
+@router.get("/read", summary="Retrieves memories from the memory bank", operation_id="readMemory")
+async def get_memories(
+    query: str = Query(description="The query")) -> str:
+    """
+    Retrieves memories from the memory bank
+    """
+    return "fake_memory";
 
